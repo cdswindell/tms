f0b21161062931931ab2958aef38113c0f4ea122
cdswindell <cdswindell@comcast.net>
2015-01-24 13:43:27 -0500

    Added javadoc

--- src/src/org/tms/api/TableRowColumnElement.java
+++ src/src/org/tms/api/TableRowColumnElement.java
@@ -9,6 +9,19 @@ public interface TableRowColumnElement
 {
     public int getIndex();
     
+    /**
+     * Sort the table by a row/column. Null elements are sorted to the end of the row/column.
+     * Numeric cells are given sort order priority. All other cells are sorted by the natural
+     * order of the cell value interpreted as a String.
+     */
     public void sort();
-    public void sort(Comparator<Cell> sorter);
+    
+    /**
+     * Sort the table by a row/column. Null elements are sorted to the end of the row/column.
+     * Numeric cells are given sort order priority. All other cells are sorted by the natural
+     * order of the cell value interpreted as a String.
+     * 
+     * @param cellSorter An instance of {@link Comparator<Cell>} that is used to sort non-null row/column cells.
+     */
+    public void sort(Comparator<Cell> cellSorter);
 }
