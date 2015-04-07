49bd50f286844d4859a67461a21f18f9bee009bf
cdswindell <cdswindell@comcast.net>
2015-04-07 14:32:58 -0400

    More test cases

--- test/src/org/tms/teq/InfixToPostfixConversionTest.java
+++ test/src/org/tms/teq/InfixToPostfixConversionTest.java
@@ -5,9 +5,10 @@ import static org.hamcrest.core.IsNull.notNullValue;
 import static org.junit.Assert.assertThat;
 
 import org.junit.Test;
+import org.tms.BaseTest;
 import org.tms.api.derivables.Token;
 
-public class InfixToPostfixConversionTest
+public class InfixToPostfixConversionTest extends BaseTest
 {
     @Test
     public final void testOperatorAssociations() 
@@ -144,5 +145,49 @@ public class InfixToPostfixConversionTest
         assertThat(t, notNullValue());
         assertThat(t.isNumeric(), is(true));
         assertThat(t.getNumericValue(), is(4096.0));
+
+        iep = new InfixExpressionParser("3^2*(5+3*(8/2+3))-8/4");
+        assertThat(iep, notNullValue());
+        
+        pr = iep.validateExpression();
+        assertThat(pr, notNullValue());
+        assertThat(pr.isSuccess(), is(true));
+        
+        psg = new PostfixStackGenerator(iep);
+        assertThat(psg, notNullValue());
+        
+        pr = psg.convertInfixToPostfix();
+        assertThat(pr, notNullValue());
+        assertThat(pr.isSuccess(), is(true));
+        
+        pse = new PostfixStackEvaluator(psg);
+        assertThat(pse, notNullValue());
+        
+        t = pse.evaluate();
+        assertThat(t, notNullValue());
+        assertThat(t.isNumeric(), is(true));
+        assertThat(t.getNumericValue(), is(232.0));
+
+        iep = new InfixExpressionParser("((3^2)*(5+(3*((8/2)+3))))-(8/4)");
+        assertThat(iep, notNullValue());
+        
+        pr = iep.validateExpression();
+        assertThat(pr, notNullValue());
+        assertThat(pr.isSuccess(), is(true));
+        
+        psg = new PostfixStackGenerator(iep);
+        assertThat(psg, notNullValue());
+        
+        pr = psg.convertInfixToPostfix();
+        assertThat(pr, notNullValue());
+        assertThat(pr.isSuccess(), is(true));
+        
+        pse = new PostfixStackEvaluator(psg);
+        assertThat(pse, notNullValue());
+        
+        t = pse.evaluate();
+        assertThat(t, notNullValue());
+        assertThat(t.isNumeric(), is(true));
+        assertThat(t.getNumericValue(), is(232.0));
     }     
 }
