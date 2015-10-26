b6800f31e7fb755f3267dc80995f11adbb92bbc9
cdswindell <cdswindell@comcast.net>
2015-10-26 11:54:51 -0400

    Added unit test for excel export

--- test/src/org/tms/io/XLSXWriterTest.java
+++ test/src/org/tms/io/XLSXWriterTest.java
@@ -1,6 +1,7 @@
 package org.tms.io;
 
 import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsNull.nullValue;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertThat;
 import static org.junit.Assert.fail;
@@ -15,9 +16,15 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 
 import org.apache.poi.EncryptedDocumentException;
+import org.apache.poi.hssf.usermodel.HSSFFormulaEvaluator;
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.apache.poi.ss.SpreadsheetVersion;
+import org.apache.poi.ss.usermodel.Comment;
+import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.ss.usermodel.WorkbookFactory;
+import org.apache.poi.xssf.usermodel.XSSFFormulaEvaluator;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.junit.Test;
 import org.tms.BaseTest;
 import org.tms.api.Access;
@@ -130,23 +137,10 @@ public class XLSXWriterTest extends BaseTest
                 );
         bos.close();
 
-        // test byte streams are the same
+        // test byte streams are the same, more or less
         byte [] output =  bos.toByteArray();
         assertNotNull(output);
-
         assertThat(this.closeTo(gold.length, output.length, 5), is(true));
-        int failures = 0;
-        int firstFailure = 0;
-        for (int i = 0; i < Math.min(gold.length, output.length); i++) {
-            if (gold[i] != output[i]) {
-                failures++;
-                if (firstFailure == 0)
-                    firstFailure = i;
-            }
-        }
-
-        // there will be failures, as new documents have date/time stamped into them
-        System.out.println("Export Table to XlsX, Failures: " + failures);
         
         // now compare exported file to expected
         File outFile = null;
@@ -158,7 +152,35 @@ public class XLSXWriterTest extends BaseTest
             
             // open workbook
             Workbook wb = WorkbookFactory.create(outFile);
-
+            assertNotNull(wb);
+            assertThat(wb.getNumberOfSheets(), is(1));
+            assertThat(wb.getSheetName(0), is("Test Table"));
+            
+            SpreadsheetVersion ssV = wb instanceof XSSFWorkbook ? SpreadsheetVersion.EXCEL2007 : SpreadsheetVersion.EXCEL97;  
+            if (ssV == SpreadsheetVersion.EXCEL2007)
+                XSSFFormulaEvaluator.evaluateAllFormulaCells((XSSFWorkbook) wb);
+            else
+                HSSFFormulaEvaluator.evaluateAllFormulaCells(wb);
+            
+            Sheet sheet = wb.getSheet("Test Table");
+            assertNotNull(sheet);
+            assertThat(sheet.getFirstRowNum(), is(0));
+            assertThat(sheet.getLastRowNum(), is(t.getNumRows()));
+            
+            org.apache.poi.ss.usermodel.Row lastRow = sheet.getRow(sheet.getLastRowNum());
+            assertNotNull(lastRow);
+            assertThat(lastRow.getLastCellNum(), is((short)(t.getNumColumns() + 1)));
+            
+            // validate column 5 (index 4) of the spreadsheet
+            vetExcelCell(sheet, 0, 4, "D Col", null, true);
+            vetExcelCell(sheet, 1, 4, 36.0, "B2 * 3.0", false);
+            vetExcelCell(sheet, 2, 4, 39000.0, "B3 * 3.0", false);
+            vetExcelCell(sheet, 3, 4, -36.0, "B4 * 3.0", false);
+            vetExcelCell(sheet, 4, 4, 52.95, "B5 * 3.0", false);
+            vetExcelCell(sheet, 5, 4, 39052.95, "sum(E2:E5)", false);
+            vetExcelCell(sheet, 6, 4, 9763.2375, "average(E2:E5)", false);
+            
+            vetExcelCell(sheet, 1, 9, "3 Row: 2 **", "(1.0 + 2.0) & \" Row: \" & (row()) & \" \" & rept(\"*\", row())", false);
         }
         catch (EncryptedDocumentException | InvalidFormatException e)
         {
@@ -167,5 +189,86 @@ public class XLSXWriterTest extends BaseTest
         finally {
             outFile.delete();
         }        
+    }
+
+    private void vetExcelCell(Sheet sheet, int rowIdx, int colIdx, Object cellVal, String formula, boolean hasComment)
+    {
+        org.apache.poi.ss.usermodel.Row eRow = sheet.getRow(rowIdx);
+        assertNotNull("Row is null: " + rowIdx, eRow);
+        assertThat(eRow.getRowNum(), is(rowIdx));
+        
+        org.apache.poi.ss.usermodel.Cell eCell = eRow.getCell(colIdx);
+        assertNotNull("Cell is null: " + colIdx, eCell);
+        assertThat(eCell.getRowIndex(), is(rowIdx));
+        assertThat(eCell.getColumnIndex(), is(colIdx));
+        
+        Object cv = fetchCellValue(eCell);
+        if (Number.class.isAssignableFrom(cv.getClass()))
+            assertThat(String.format("cell value (%f) not within tolerance of expected (%f)", (double)cellVal, ((Number)cv).doubleValue()),
+                    closeTo(cellVal, ((Number)cv).doubleValue(), 0.0000001), is(true));
+        else
+            assertThat(cv, is(cellVal));   
+        
+        String excelFormula = eCell.getCellType() == org.apache.poi.ss.usermodel.Cell.CELL_TYPE_FORMULA ?
+                                    eCell.getCellFormula() : null;
+        if (formula == null)
+            assertThat("No formula expected: " + excelFormula, excelFormula, nullValue());
+        else
+            assertThat(excelFormula, is(formula));
+        
+        Comment comment = eCell.getCellComment();
+        if (hasComment)
+            assertNotNull("Expected comment", comment);
+        else
+            assertThat("No comment expected", comment, nullValue());
+    }
+    
+    private Object fetchCellValue(org.apache.poi.ss.usermodel.Cell eC) 
+    {
+        if (eC == null)
+            return null;
+
+        // decode based on the cell type
+        int cellType = eC.getCellType();
+        switch(cellType) {
+            case org.apache.poi.ss.usermodel.Cell.CELL_TYPE_BOOLEAN:
+                return eC.getBooleanCellValue();
+
+            case org.apache.poi.ss.usermodel.Cell.CELL_TYPE_NUMERIC:
+                return eC.getNumericCellValue();
+
+            case org.apache.poi.ss.usermodel.Cell.CELL_TYPE_STRING:
+                return eC.getRichStringCellValue().getString();
+
+            case org.apache.poi.ss.usermodel.Cell.CELL_TYPE_FORMULA:
+                return fetchFormulaCellValue(eC);
+
+            default:
+                return null;
+        }
+    }
+
+    private Object fetchFormulaCellValue(org.apache.poi.ss.usermodel.Cell eC) 
+    {
+        // parse formula value
+        Object cv = null;
+        try {
+            cv = eC.getNumericCellValue();
+        }
+        catch (IllegalStateException ise) {
+            try {
+                cv = eC.getStringCellValue();
+            }
+            catch (IllegalStateException iseStr) {
+                try {
+                    cv = eC.getBooleanCellValue();
+                }
+                catch (IllegalStateException iseBool) {
+                    cv = ErrorCode.NaN;
+                }
+            }
+        }
+
+        return cv;
     }
 }
