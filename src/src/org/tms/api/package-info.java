79c2a03b78aa79615be734b3072e6fce4a5f1dc0
cdswindell <cdswindell@comcast.net>
2015-10-28 13:28:00 -0400

    Updated javadoc

--- src/src/org/tms/api/package-info.java
+++ src/src/org/tms/api/package-info.java
@@ -1,5 +1,6 @@
 /**
- * Contains the core apis to manipulate TMS Tables.
+ * TMS is a framework for ad-hoc analysis of complex data. This package contains the core apis to 
+ * create, manage, and manipulate TMS Tables.
  * 
  * @since {@value org.tms.api.utils.ApiVersion#INITIAL_VERSION_STR}
  * @version {@value org.tms.api.utils.ApiVersion#CURRENT_VERSION_STR}
