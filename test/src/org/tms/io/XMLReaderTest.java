6e16b347832ffcd63735fa9280ac1f1889392eac
cdswindell <cdswindell@comcast.net>
2015-11-04 17:50:08 -0500

    Added unit tests for XML Import/Export

--- test/src/org/tms/io/XMLReaderTest.java
+++ test/src/org/tms/io/XMLReaderTest.java
@@ -7,25 +7,39 @@ import static org.junit.Assert.fail;
 
 import java.io.IOException;
 
+import org.junit.AfterClass;
 import org.junit.Test;
 import org.tms.BaseTest;
+import org.tms.api.Access;
+import org.tms.api.Cell;
 import org.tms.api.Column;
 import org.tms.api.Row;
+import org.tms.api.Subset;
 import org.tms.api.Table;
+import org.tms.api.TableContext;
+import org.tms.api.factories.TableContextFactory;
+import org.tms.api.factories.TableFactory;
 import org.tms.api.io.XMLOptions;
+import org.tms.tds.TdsUtils;
 
 public class XMLReaderTest extends BaseTest
 {
+    @AfterClass
+    static public void cleanup()
+    {
+        TableContext tc = TableContextFactory.fetchDefaultTableContext();
+        TdsUtils.clearGlobalTagCache(tc);
+    }
+    
     private static final String SAMPLE1 = "sample1.csv";
-    private static final String SAMPLE2 = "sample2.csv";
-    private static final String SAMPLE3 = "sample3.csv";
+    private static final String ExportTableGold = "testExportTable.xml";
     
     @Test
     public final void testCSVReaderConstructor()
     {
-        XMLReader r = new XMLReader(qualifiedFileName(SAMPLE1), XMLOptions.Default); 
+        XMLReader r = new XMLReader(qualifiedFileName(ExportTableGold), XMLOptions.Default); 
         assertNotNull(r);
-        assertThat(r.getFileName(), is(SAMPLE1));
+        assertThat(r.getFileName(), is(ExportTableGold));
         assertThat(r.isRowNames(), is(true));
         assertThat(r.isColumnNames(), is(true));
     }
@@ -33,7 +47,35 @@ public class XMLReaderTest extends BaseTest
     @Test
     public final void testParse() 
     {
-        XMLReader r = new XMLReader("foo.xml", XMLOptions.Default); 
+        // create the reference table
+        Table gst = TableFactory.importCSV(qualifiedFileName(SAMPLE1), true, true);
+        assertNotNull(gst);
+        gst.setLabel("Test XML & Export Table");
+        gst.tag("red", "green");
+        
+        Column gsc3 = gst.getColumn(3);
+        Cell r1c3 = gst.getCell(gst.getRow(1), gsc3);
+        r1c3.clear();
+        r1c3.setLabel("foo");
+        r1c3.setUnits("mph");
+        r1c3.setDescription("Cell Description");
+        
+        Subset s1 = gst.addSubset(Access.ByLabel, "Excluded Cols");
+        Subset s2 = gst.addSubset(Access.ByLabel, "Some Cols");
+        s2.tag("red", "subset");
+        
+        Column gsc = gst.addColumn();
+        s1.add(gsc);
+        s2.add(gsc, gsc3);
+        
+        gsc = gst.addColumn();
+        s2.add(gsc);
+
+        gsc.setDerivation("col 1 * col 1");
+        gsc.tag("derived", "calculated", "no-user-data");
+        
+        // now read the xml
+        XMLReader r = new XMLReader(qualifiedFileName(ExportTableGold), XMLOptions.Default); 
         assertNotNull(r);
         
         try
@@ -41,9 +83,9 @@ public class XMLReaderTest extends BaseTest
             Table t = r.parse();
             assertNotNull(t);
             
-            assertThat(t.getNumRows(), is(4));
-            assertThat(t.getNumColumns(), is(3));
-            assertThat(t.getNumCells(), is(12));
+            assertThat(t.getNumRows(), is(gst.getNumRows()));
+            assertThat(t.getNumColumns(), is(gst.getNumColumns()));
+            assertThat(t.getNumCells(), is(gst.getNumCells()));
             
             assertThat(t.getColumn(1).getLabel(), is("A"));
             assertThat(t.getColumn(2).getLabel(), is("B"));
@@ -55,7 +97,9 @@ public class XMLReaderTest extends BaseTest
             for (Row row : t.rows()) {
                 assertThat(Number.class.isAssignableFrom(t.getCellValue(row, c1).getClass()), is(true));
                 assertThat(row.getLabel(), is(t.getCellValue(row, c2) + " Row"));
-                assertThat(Boolean.class.isAssignableFrom(t.getCellValue(row, c3).getClass()), is(true));
+                
+                if (row.getIndex() != 1)
+                    assertThat(Boolean.class.isAssignableFrom(t.getCellValue(row, c3).getClass()), is(true));
             }
         }
         catch (IOException e)
--- test/src/org/tms/io/XMLWriterTest.java
+++ test/src/org/tms/io/XMLWriterTest.java
@@ -1,10 +1,15 @@
 package org.tms.io;
 
+import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertThat;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
-import org.apache.poi.ss.formula.functions.T;
 import org.junit.AfterClass;
 import org.junit.Test;
 import org.tms.BaseTest;
@@ -29,36 +34,57 @@ public class XMLWriterTest extends BaseTest
     }
     
     private static final String SAMPLE1 = "sample1.csv";
+    private static final String ExportTableGold = "testExportTable.xml";
     
     @Test
     public final void testExportXml() throws IOException
     {
-        Table t = TableFactory.importCSV(qualifiedFileName(SAMPLE1), true, true);
-        assertNotNull(t);
-        t.setLabel("Test XML & Export Table");
-        t.tag("red", "green");
+        /*
+         * Note: If you change this test, be sure to update
+         * the gold standard file ExportTableGold
+         */
+        Path path = Paths.get(qualifiedFileName(ExportTableGold));
+        byte[] gold = Files.readAllBytes(path);  
+
+        assertNotNull(gold);
+        assertThat(gold.length > 0, is(true));
         
-        Column c3 = t.getColumn(3);
-        Cell r1c3 = t.getCell(t.getRow(1), c3);
+        // create new XML, it should match the gold standard
+        Table gst = TableFactory.importCSV(qualifiedFileName(SAMPLE1), true, true);
+        assertNotNull(gst);
+        gst.setLabel("Test XML & Export Table");
+        gst.tag("red", "green");
+        
+        Column gsc3 = gst.getColumn(3);
+        Cell r1c3 = gst.getCell(gst.getRow(1), gsc3);
         r1c3.clear();
         r1c3.setLabel("foo");
         r1c3.setUnits("mph");
         r1c3.setDescription("Cell Description");
         
-        Subset s1 = t.addSubset(Access.ByLabel, "Excluded Cols");
-        Subset s2 = t.addSubset(Access.ByLabel, "Some Cols");
+        Subset s1 = gst.addSubset(Access.ByLabel, "Excluded Cols");
+        Subset s2 = gst.addSubset(Access.ByLabel, "Some Cols");
         s2.tag("red", "subset");
         
-        Column c = t.addColumn();
-        s1.add(c);
-        s2.add(c, c3);
+        Column gsc = gst.addColumn();
+        s1.add(gsc);
+        s2.add(gsc, gsc3);
         
-        c = t.addColumn();
-        s2.add(c);
+        gsc = gst.addColumn();
+        s2.add(gsc);
 
-        c.setDerivation("col 1 * col 1");
-        c.tag("derived", "calculated", "no-user-data");
-        t.export("foo.xml", XMLOptions.Default.withIgnoreEmptyColumns(true));
+        gsc.setDerivation("col 1 * col 1");
+        gsc.tag("derived", "calculated", "no-user-data");
         
+        // create output stream
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        gst.export(bos, XMLOptions.Default);
+        bos.close();
+
+        // test byte streams are the same
+        byte [] output =  bos.toByteArray();
+        assertNotNull(output);
+
+        assertThat(gold.length, is(output.length));       
     }
 }
